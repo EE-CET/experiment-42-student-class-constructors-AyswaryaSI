15ba309601f49fd1653cfb9f997c70745f779d04
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-06 10:15:35 +0530

    Update StudentClass.java

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -1,37 +1,37 @@
 import java.util.Scanner;
 
-class StudentClass{
-      String name;
-      int    rollNo;
+public class StudentClass {
 
-      StudentClass(String name,int rollNo){
-              this.name = name;
-              this.rollNo = rollNo;
-     }
-     void display(int studentNumber){
-          System.out.println("Student "+studentNumber+"details:Name: "+name+"Roll: "+rollNo);
-     }
-}
-          
+    String name;
+    int rollNo;
 
-public class Main {
+    StudentClass(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    void display(int studentNumber) {
+        System.out.println("Student " + studentNumber + " details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + rollNo);
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         String name1 = sc.nextLine();
-        int rollNo1  = sc.nextInt();
+        int rollNo1 = sc.nextInt();
+        sc.nextLine();
         String name2 = sc.nextLine();
-        int rollNo2 =  sc.nextInt();
-   
-        StudentClass s1 = new StudentClass(name1, rollNo2);
+        int rollNo2 = sc.nextInt();
+
+        StudentClass s1 = new StudentClass(name1, rollNo1);
         StudentClass s2 = new StudentClass(name2, rollNo2);
 
-       s1.display(1);
-       s2.display(2);
-   
+        s1.display(1);
+        s2.display(2);
 
-      sc.close();
-   }
+        sc.close();
+    }
 }
-
