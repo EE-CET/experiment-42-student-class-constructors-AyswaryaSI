c987bbd6f3086f4ecf35b441cccfacdf69f83870
AyswaryaSI <ayswaryasi293@gmail.com>
2026-02-06 10:17:32 +0530

    Refactor display method to remove student number

--- src/StudentClass.java
+++ src/StudentClass.java
@@ -4,16 +4,13 @@ public class StudentClass {
 
     String name;
     int rollNo;
-
     StudentClass(String name, int rollNo) {
         this.name = name;
         this.rollNo = rollNo;
     }
 
-    void display(int studentNumber) {
-        System.out.println("Student " + studentNumber + " details:");
-        System.out.println("Name: " + name);
-        System.out.println("Roll: " + rollNo);
+    void display() {
+        System.out.println("Name: " + name + " Roll: " + rollNo);
     }
 
     public static void main(String[] args) {
@@ -22,15 +19,16 @@ public class StudentClass {
 
         String name1 = sc.nextLine();
         int rollNo1 = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); // clear buffer
+
         String name2 = sc.nextLine();
         int rollNo2 = sc.nextInt();
 
         StudentClass s1 = new StudentClass(name1, rollNo1);
         StudentClass s2 = new StudentClass(name2, rollNo2);
 
-        s1.display(1);
-        s2.display(2);
+        s1.display();
+        s2.display();
 
         sc.close();
     }
